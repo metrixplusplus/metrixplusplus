6bfeb8f94b7e6842121246e215c949bb570917cc
avkonst <avkonst@41fc585f-aad7-44d6-ae36-bab1cf7f48e8>
2013-08-11 20:13:17 +0000

    tests for runable api.

---
+++ mainline/tests/system/test_api_tutorial/sources_changed/test.cpp
@@ -0,0 +1,35 @@
+
+
+const unsigned int c = 3;
+unsigned int c = 3;
+int c = 3;
+
+
+int main(int a = 1, int b = -2)
+{
+	const unsigned int c = 3;
+
+	for (int i = 0; i < 100; ++i)
+	{
+		// modified
+	}
+	while (1)
+	{
+		a = 4;
+	}
+}
+
+int simple_func()
+{
+	const unsigned int c = c;
+}
+
+int added()
+{
+
+}
+
+int added2()
+{
+
+}
