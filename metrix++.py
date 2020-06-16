6e564969b106005a3e938cd2c2bcf8c9369cba48
prozessorkern <opel1000@gmx.net>
2020-06-16 22:34:47 +0200

    added first try of setup script for pypi moved all code into its own package added setup.py file moved the tests out of the way for now

--- metrix++.py
+++ metrix++.py
@@ -6,5 +6,5 @@
 #    
 
 if __name__ == '__main__':
-    import metrixpp
+    from metrixpp import metrixpp
     metrixpp.start()
---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="metrixpp-prozessorkern", # Replace with your own username
+    version="0.0.3",
+    author="Stefan Strobel",
+    author_email="stefan.strobel@shimatta.net",
+    description="Metrix++ is an extendable tool for code metrics collection and analysis.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/metrixplusplus/metrixplusplus",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    package_data={
+        'metrixpp.mpp': ['*.ini'],
+        'metrixpp.ext.std': ['*.ini'],
+        'metrixpp.ext.std.tools': ['*.ini'],
+        'metrixpp.ext.std.code': ['*.ini']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        'Documentation': 'https://metrixplusplus.github.io/',
+        'Source': 'https://github.com/metrixplusplus/metrixplusplus',
+        'Tracker': 'https://github.com/metrixplusplus/metrixplusplus/issues',
+    },
+    entry_points={
+        'console_scripts': [
+            'metrix++ = metrixpp:main'
+        ]
+    },
+    python_requires='>=3.5',
+)
