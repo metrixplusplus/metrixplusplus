e5fb43a3dc167db8602c84f2db28f82ca2e3ca8d
prozessorkern <opel1000@gmx.net>
2020-06-17 20:48:39 +0200

    added first try of packaging action

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="metrixpp-prozessorkern", # Replace with your own username
+    name="metrixpp",
     version="0.0.1",
     author="Stefan Strobel",
     author_email="stefan.strobel@shimatta.net",
