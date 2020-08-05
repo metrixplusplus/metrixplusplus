e746fc133f163c0ad65bc7cc18d1ea4a8bdc701b
Marina Konstantinova <mskonst@gmail.com>
2020-08-05 20:19:33 +1200

    .

--- metrix++.py
+++ metrix++.py
@@ -1,5 +1,5 @@
 #
-#    Metrix++, Copyright 2009-2019, Metrix++ Project
+#    Metrix++, Copyright 2009-2020, Metrix++ Project
 #    Link: https://github.com/metrixplusplus/metrixplusplus
 #    
 #    This file is a part of Metrix++ Tool.
